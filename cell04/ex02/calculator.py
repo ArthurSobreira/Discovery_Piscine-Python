7cc98b2eb498f8b34d8204f5c990d83624ddb398
Arthur Sobreira <tutusobreira@gmail.com>
2024-08-07 11:31:37 -0300

    Update calculator.py

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
 
 def	main() -> None:
-	first_number: int = int(input("Give me the first number: "))
-	second_number: int = int(input("Give me the second number: "))
+	try:
+		first_number: int = int(input("Give me the first number: "))
+		second_number: int = int(input("Give me the second number: "))
 
-	print("Thank you!")
-	
-	list_of_operations: list[str] = ['+', '-', '/', '*']
-	for operation in list_of_operations:
-		print(f"{first_number} + {second_number}")
-		if (operation == '+'):
-			print(f"{first_number} + {second_number} = {first_number + second_number}")
-		elif (operation == '-'):
-			print(f"{first_number} - {second_number} = {first_number - second_number}")
-		elif (operation == '/'):
-			print(f"{first_number} / {second_number} = {first_number // second_number}")
-		elif (operation == '*'):
-			print(f"{first_number} * {second_number} = {first_number * second_number}")
+		print("Thank you!")
+		
+		list_of_operations: list[str] = ['+', '-', '/', '*']
+		for operation in list_of_operations:
+			print(f"{first_number} {operation} {second_number}", end=" = ")
+			if (operation == '+'):
+				print(first_number + second_number)
+			elif (operation == '-'):
+				print(first_number - second_number)
+			elif (operation == '/'):
+				print(first_number // second_number)
+			elif (operation == '*'):
+				print(first_number * second_number)
+
+	except ValueError:
+		print("Please enter a valid number.")
+		return
 	
 if __name__ == "__main__":
 	main()
