132034db69d1d3bc453cb98ceee1780a4deba702
Arthur Sobreira <tutusobreira@gmail.com>
2024-08-07 12:24:57 -0300

    Update up_low.py

---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def	main() -> None:
+	user_input: str = input("Enter a string: ")
+	print(f"Case swapped: {user_input.swapcase()}")
+    
+if __name__ == "__main__":
+	main()
