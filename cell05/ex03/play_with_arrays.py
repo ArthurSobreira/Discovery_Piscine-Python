4e374575d96601d932f405a63d76d4d5114d44a2
Arthur Sobreira <tutusobreira@gmail.com>
2024-08-07 15:35:07 -0300

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -2,8 +2,7 @@
 
 def	main() -> None:
 	original_array: list[int] = [2, 8, 9, 48, 8, 22, -12, 2]
-	unique_array: list[int] = list(set(original_array))
-	new_array: list[int] = {x + 2 for x in unique_array if x > 5}
+	new_array: list[int] = {x + 2 for x in set(original_array) if x > 5}
 
 	print(f"Original array: {original_array}")
 	print(f"New array: {new_array}")	
