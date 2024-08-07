6c8d5847cc4de05bc981e8378e8738e3dd9581c5
Arthur Sobreira <tutusobreira@gmail.com>
2024-08-07 11:22:40 -0300

    Update age.py

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 def	main() -> None:
-	age: int = int(input("Please tell me your age: "))
-	print(f"You are currently {age} years old.")
-	print(f"In 10 years, you'll be {age + 10} years old.")
-	print(f"In 20 years, you'll be {age + 20} years old.")
-	print(f"In 30 years, you'll be {age + 30} years old.")
+	try:
+		age: int = int(input("Please tell me your age: "))
+		print(f"You are currently {age} years old.")
+	
+		ages: list[int] = [age + 10, age + 20, age + 30]
+		for i in range(3):
+			print(f"In {(i + 1) * 10} years, you will be {ages[i]} years old.")
+	except ValueError:
+		print("Please enter a valid age.")
+		return
 
 if __name__ == "__main__":
 	main()
