ef399a28267579081b3f3354b93e366ab5992917
Arthur Sobreira <tutusobreira@gmail.com>
2024-08-08 17:58:21 -0300

    Update family_affairs.py

--- cell07/ex01/family_affairs.py
+++ cell07/ex01/family_affairs.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+def	find_the_redheads(family: dict) -> list:
+	return [key.capitalize() for key, value in family.items() \
+		if value == "red"]
+
+def	main() -> None:
+	dupont_family = {
+	"florian": "red",
+	"marie": "blond",
+	"virginie": "brunette",
+	"david": "red",
+	"franck": "red"
+	}
+
+	print(find_the_redheads(dupont_family))
+
+if __name__ == "__main__":
+	main()
