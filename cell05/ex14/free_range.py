806dc50b709298b73d8630f42343b4722bfadc17
Arthur Sobreira <tutusobreira@gmail.com>
2024-08-08 12:10:21 -0300

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -7,13 +7,13 @@ def	main() -> None:
 		print("none")
 		return
 
-	first: int = int(sys.argv[1])
-	last: int = int(sys.argv[2])
+	begin: int = int(sys.argv[1])
+	end: int = int(sys.argv[2])
 	
-	if first < last:
-		print(list(range(first, last + 1)))
+	if begin < end:
+		print(list(range(begin, end + 1)))
 	else:
-		print(list(range(first, last - 1, -1)))
+		print(list(range(begin, end - 1, -1)))
 
 if __name__ == "__main__":
 	main()
